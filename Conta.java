9e53338452a239ad6d51e4ab643296a93cc06aeb
Delphino Luciani <125306330+dlpaf@users.noreply.github.com>
2024-10-07 23:18:00 -0300

    Create Conta.java para o transaction
    Necessário ter conta para ter transação

---
+++ Conta.java
@@ -0,0 +1,29 @@
+public class Conta {
+    private double saldo;
+
+    public Conta() {
+        this.saldo = 0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$ " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor inválido para depósito.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido.");
+        }
+    }
+}
