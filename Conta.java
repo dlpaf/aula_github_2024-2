ba66801449ea22cd42b56dc3f49657a9eb8c79db
Delphino Luciani <125306330+dlpaf@users.noreply.github.com>
2024-10-07 23:21:05 -0300

    Update Conta.java com a lógica de transação

--- Conta.java
+++ Conta.java
@@ -9,21 +9,15 @@ public class Conta {
         return saldo;
     }
 
-    public void depositar(double valor) {
-        if (valor > 0) {
-            saldo += valor;
-            System.out.println("Depósito de R$ " + valor + " realizado com sucesso.");
-        } else {
-            System.out.println("Valor inválido para depósito.");
-        }
-    }
-
-    public void sacar(double valor) {
+    public boolean transferir(Conta destino, double valor) {
         if (valor > 0 && valor <= saldo) {
-            saldo -= valor;
-            System.out.println("Saque de R$ " + valor + " realizado com sucesso.");
+            this.sacar(valor);
+            destino.depositar(valor);
+            System.out.println("Transferência de R$ " + valor + " realizada com sucesso.");
+            return true;
         } else {
             System.out.println("Saldo insuficiente ou valor inválido.");
+            return false;
         }
     }
 }
