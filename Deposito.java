546e5a4e83974e7c8f3dbf171820e35b94e91eea
Neto <139978998+rngneto@users.noreply.github.com>
2024-10-07 23:26:18 -0300

    Adicionando método de depósito. Close issue #9

---
+++ Deposito.java
@@ -0,0 +1,20 @@
+public class Deposito {
+    private double saldo;
+
+    public Deposito(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
